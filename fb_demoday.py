a29989865eaa728a10038f784159eb8c6e2bc65e
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-08 17:42:04 +0100

    Add files via upload

--- fb_demoday.py
+++ fb_demoday.py
@@ -30,25 +30,52 @@ Bla bla bla information about facebook bla bla bla"""
 df = pd.read_csv('data_clean_2.csv', index_col=0)
 color_list = ['DarkCyan', 'GreenYellow', 'Orchid']
 
-# View per country
-st.title('KPI behaviour according to country')
-df_behaviour_countries = df.groupby(['country','date']).agg(
+st.title('KPIs evolutions')
+# Static plots in two columns
+col1, col2 = st.columns(2)
+
+with col1:
+    # View per country
+    st.subheader('Per country')
+    df_behaviour_countries = df.groupby(['country','date']).agg(
                                         {'spend $': np.sum,
                                          'revenue $': np.sum,
                                         'purchase': np.sum}
                                         ).reset_index()
 
-all_countries = df_behaviour_countries['country'].unique().tolist()
-options = st.selectbox(
- 'Which country are you interested in diving in?', all_countries)
+    all_countries = df_behaviour_countries['country'].unique().tolist()
+    options = st.selectbox('Which country are you interested in diving in?', all_countries)
 
-# Filter the information for this port specifically
-ind_country = df_behaviour_countries[df_behaviour_countries['country']== options]
+    # Filter the information for this port specifically
+    ind_country = df_behaviour_countries[df_behaviour_countries['country']== options]
 
-fig = px.bar(ind_country, 
+    fig1 = px.bar(ind_country, 
                  x = "date", 
                  y = ["spend $","revenue $"])
-fig.update_yaxes(visible=False, fixedrange=True)
+    fig1.update_yaxes(visible=False, fixedrange=True)
 
-fig.update_layout(barmode='group')
-st.plotly_chart(fig)
+    fig1.update_layout(barmode='group')
+    st.plotly_chart(fig1)
+
+with col2:    
+    st.subheader('Per country')
+    df_behaviour_target = df.groupby(['target type','date']).agg(
+                                        {'spend $': np.sum,
+                                         'revenue $': np.sum,
+                                        'purchase': np.sum}
+                                        ).reset_index()
+
+    all_target = df_behaviour_target['target type'].unique().tolist()
+    options = st.selectbox('Which target type are you interested in diving in?', all_target)
+
+    # Filter the information for this port specifically
+    ind_target = df_behaviour_target[df_behaviour_target['target type']== options]
+
+    fig2 = px.bar(ind_target, 
+                 x = "date", 
+                 y = ["spend $","revenue $"])
+    fig2.update_yaxes(visible=False, fixedrange=True)
+
+    fig2.update_layout(barmode='group')
+    st.plotly_chart(fig2)
+    
