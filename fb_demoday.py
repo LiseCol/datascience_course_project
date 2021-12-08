730eb2d11ebacf77cc8b509e48ff393eae0fed26
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-08 17:12:06 +0100

    Add files via upload

--- fb_demoday.py
+++ fb_demoday.py
@@ -7,33 +7,50 @@ from PIL import Image
 import numpy as np
 import plotly.express as px
 
-def get_percentages(df,name_column):
-    percentage = df[name_column].value_counts(normalize=True)*100
-    percentage = percentage.reset_index()
-    percentage.columns = [name_column,'percentage']
-    percentage = percentage.sort_values(by=name_column)
-    return percentage
-
 ##############################################################################################
 # PAGE STYLING
 ##############################################################################################
-st.set_page_config(page_title="Titanic Dashboard ", 
-                   page_icon=":ship:",
+st.set_page_config(page_title="Best facebook reporting ever ", 
+                   page_icon=":star:",
                    layout='wide')
                    
-st.title("Welcome to the ***Titanic*** dashboard! : :star:")
+st.title("Welcome to my dashboard! : :star:")
 """
 MY TEXT MY TEXT
 """
 # Page styling
 
-st.markdown("Let's ask the following question: ***'Can we use Python to retrive information from the titanic?' ***")
+st.markdown("Let's make some test: ***'hello' ***")
 
-st.header("**Overall information from Titanic**")
+st.header("**Overall information about my dashboard**")
 """
-Bla bla bla information from Titanic bla bla bla"""
-
+Bla bla bla information about facebook bla bla bla"""
 
 # Data loading and first checks
 df = pd.read_csv('data_clean_2.csv', index_col=0)
 color_list = ['DarkCyan', 'GreenYellow', 'Orchid']
+
+# Static plots in two columns
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    df_behaviour_countries = df.groupby(['country','date']).agg(
+                                        {'spend $': np.sum,
+                                         'revenue $': np.sum,
+                                        'purchase': np.sum}
+                                        ).reset_index()
+    def split_country(country):
+    return df_behaviour_countries[df_behaviour_countries['country']==country]
+
+    st.subheader('KPI behaviour according to country')
+    fig = fig = px.bar(split_country('ES'), 
+                 x = "date", 
+                 y = ["spend $","revenue $"],title=Spain)
+    fig.update_yaxes(visible=False, fixedrange=True)
+
+    fig.update_layout(barmode='group')
+    st.plotly_chart(fig)
+
+with col2:
+    with col2:
+    st.markdown('test')
