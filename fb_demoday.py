a81b461bffb90d9f46c5245f5f5ed89c8f3d9d5d
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-10 17:47:59 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -7,21 +7,14 @@ from PIL import Image
 import numpy as np
 import plotly.express as px
 
-## Page config ##
-st.set_page_config(page_title="Facebook ad Report", 
-                   page_icon=":bar_chart:",
-                   layout='wide')
 
-# Page title                   
-st.title("Facebook ad Report :bar_chart:")
-
-# Load the data
-@st.cache(allow_output_mutation=True)
+# Define functions
+@st.cache
 def load_data():
         df = pd.read_csv('data_clean_2.csv', index_col=0)
         return  df
 
-color_list = ['DarkCyan', 'GreenYellow', 'Orchid']
+#color_list = ['DarkCyan', 'GreenYellow', 'Orchid']
 
 # Define functions
 def groupby_all(variable):
@@ -38,86 +31,99 @@ def groupby_all(variable):
                                       'daily budget': np.sum,
                                      'currency':pd.Series.mode}).reset_index()
 
-# Add sth into sidebar
-text = """
+def main():
+    ## Page config ##
+    st.set_page_config(page_title="Facebook ad Report", 
+                   page_icon=":bar_chart:",
+                   layout='wide')
+
+    # Page title                   
+    st.title("Facebook ad Report :bar_chart:")
+
+    # Add sth into sidebar
+    text = """
     :arrow_forward: **To start**: \n
     Don't forget to select your favorite filters\n
     ---------------------\n
     `This dashboard is based on a sample of 2 months facebook historical data.`\n
     ---------------------
     """
-st.sidebar.markdown(text)
+    st.sidebar.markdown(text)
+
 
     # Selectbox : View of the dataframe
-st.sidebar.subheader("FILTERS")
+    st.sidebar.subheader("FILTERS")
 
-status = st.sidebar.selectbox('Select your favorite view:',["Performance per country",
+    status = st.sidebar.selectbox('Select your favorite view:',["Performance per country",
                                                             "Performance per target type",
                                                             "Daily view"])
 
-status2 = st.sidebar.radio("Select the prefered currency :",("Local currency","USD"))
+    status2 = st.sidebar.radio("Select the prefered currency :",("Local currency","USD"))
 
-if status == "Performance per country":
-    if status2 == "Local currency":
-        st.subheader("Performance per country")
-        df = load_data()
-        groupby_all('country')['CPA'] = round(groupby_all('country')['spend']/groupby_all('country')['purchase'],2)
-        st.dataframe(groupby_all('country'))
+    if status == "Performance per country":
+        if status2 == "Local currency":
+            st.subheader("Performance per country")
+            df = load_data()
+            groupby_all('country')['CPA'] = round(groupby_all('country')['spend']/groupby_all('country')['purchase'],2)
+            st.dataframe(groupby_all('country'))
 
-elif status == "Performance per target type":
-    st.subheader("Performance per target type")
-    st.dataframe(groupby_all('target type'))
+    elif status == "Performance per target type":
+        st.subheader("Performance per target type")
+        st.dataframe(groupby_all('target type'))
         
-elif status == "Daily view":
-    st.subheader("Daily view")
-    st.dataframe(groupby_all('date'))
+    elif status == "Daily view":
+        st.subheader("Daily view")
+        st.dataframe(groupby_all('date'))
 
                                                                                   
-# Static plots in two columns
-col1, col2 = st.columns(2)
+    # Static plots in two columns
+    col1, col2 = st.columns(2)
 
-with col1:
-    # View per country
-    st.subheader('Per country')
-    df_behaviour_countries = df.groupby(['country','date']).agg(
+    with col1:
+        # View per country
+        st.subheader('Per country')
+        df_behaviour_countries = df.groupby(['country','date']).agg(
                                         {'spend $': np.sum,
                                          'revenue $': np.sum,
                                         'purchase': np.sum}
                                         ).reset_index()
 
-    all_countries = df_behaviour_countries['country'].unique().tolist()
-    options = st.selectbox('Which country are you interested in diving in?', all_countries)
+        all_countries = df_behaviour_countries['country'].unique().tolist()
+        options = st.selectbox('Which country are you interested in diving in?', all_countries)
 
-    # Filter the information for this port specifically
-    ind_country = df_behaviour_countries[df_behaviour_countries['country']== options]
+        # Filter the information for this port specifically
+        ind_country = df_behaviour_countries[df_behaviour_countries['country']== options]
 
-    fig1 = px.bar(ind_country, 
+        fig1 = px.bar(ind_country, 
                  x = "date", 
                  y = ["spend $","revenue $"])
-    fig1.update_yaxes(visible=False, fixedrange=True)
+        fig1.update_yaxes(visible=False, fixedrange=True)
 
-    fig1.update_layout(barmode='group')
-    st.plotly_chart(fig1)
+        fig1.update_layout(barmode='group')
+        st.plotly_chart(fig1)
 
-with col2:    
-    st.subheader('Per target type')
-    df_behaviour_target = df.groupby(['target type','date']).agg(
+    with col2:    
+        st.subheader('Per target type')
+        df_behaviour_target = df.groupby(['target type','date']).agg(
                                         {'spend $': np.sum,
                                          'revenue $': np.sum,
                                         'purchase': np.sum}
                                         ).reset_index()
 
-    all_target = df_behaviour_target['target type'].unique().tolist()
-    options = st.selectbox('Which target type are you interested in diving in?', all_target)
+        all_target = df_behaviour_target['target type'].unique().tolist()
+        options = st.selectbox('Which target type are you interested in diving in?', all_target)
 
-    # Filter the information for this port specifically
-    ind_target = df_behaviour_target[df_behaviour_target['target type']== options]
+        # Filter the information for this port specifically
+        ind_target = df_behaviour_target[df_behaviour_target['target type']== options]
 
-    fig2 = px.bar(ind_target, 
+        fig2 = px.bar(ind_target, 
                  x = "date", 
                  y = ["spend $","revenue $"])
-    fig2.update_yaxes(visible=False, fixedrange=True)
+        fig2.update_yaxes(visible=False, fixedrange=True)
 
-    fig2.update_layout(barmode='group')
-    st.plotly_chart(fig2)
+        fig2.update_layout(barmode='group')
+        st.plotly_chart(fig2)
+        
+if __name__ == "__main__":
+    main()
     
