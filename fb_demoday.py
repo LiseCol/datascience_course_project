68463ec80f5d51293b3cd6946a542d38bab6ae02
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-12 17:37:46 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -115,8 +115,8 @@ def main():
         #df_daily['date']= pd.to_datetime(df_daily['date'])
         min_date = datetime.date(2021,11,1)
         max_date = datetime.date(2021,11,17)
-        st.date_input("Pick a date", min_value=min_date, max_value=max_date)
-        
+        a = st.date_input("Pick a date", min_value=min_date, max_value=max_date)
+        st.write(a)
         #if min_date < max_date:
        #     pass
         #else:
