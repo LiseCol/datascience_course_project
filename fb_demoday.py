7858c54685a7294bd5f5b6993b118ee214c23cab
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-12 16:38:16 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -1,7 +1,6 @@
 #Import common modules
 import streamlit as st
 import pandas as pd
-pd.options.mode.chained_assignment = None
 from PIL import Image
 import numpy as np
 import plotly.express as px
