fa4b741a1cbc279672e2d6f73fbbef0c96dca0cf
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-12 17:47:59 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -113,8 +113,8 @@ def main():
         st.subheader("Daily view - USD")
         df_daily = groupby_all('date','usd')
         df_daily['date']= pd.to_datetime(df_daily['date'])
-        min_date = datetime.date(2021,11,1)
-        max_date = datetime.date(2021,11,17)
+        min_date = datetime.date#(2021,11,1)
+        max_date = datetime.date#(2021,11,17)
         st.date_input("Pick a date", value=(min_date, max_date))
         if min_date < max_date:
             pass
