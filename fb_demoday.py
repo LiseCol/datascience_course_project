09c1b18230e93c11d0b077a0e85d3239cc6c0183
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-13 12:51:02 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -110,9 +110,9 @@ def main():
         st.write("## ... and here's a row of them")
         metric_row(
         {
-        {"Metric 1": 100, "1.2 °F"},
-        {"Metric 2": 200, "-8%"},
-        {"Metric 3": 300,"4%"}
+        "Metric 1": 100,
+        "Metric 2": 200,
+        "Metric 3": 300,
         },
         )
         
