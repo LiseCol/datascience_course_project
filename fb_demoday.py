1fa37f044ff0fbfe288ac2be71f9f49a575f2adc
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-14 17:47:47 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -1,5 +1,6 @@
 #Import common modules
 import streamlit as st
+import hydralit_components as hc
 import pandas as pd
 pd.options.mode.chained_assignment = None
 from PIL import Image
@@ -88,6 +89,26 @@ def groupby_all(variable1,variable2,cur):
             return df_var
 
 def main():
+    
+    # specify the primary menu definition
+    menu_data = [
+        {'icon': "far fa-copy", 'label':"Left End"},
+        {'id':'Copy','icon':"🐙",'label':"Copy"},
+        {'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
+        {'icon': "far fa-address-book", 'label':"Book"},
+        {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
+        {'icon': "far fa-clone", 'label':"Component"},
+        {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
+        {'icon': "far fa-copy", 'label':"Right End"},
+            ]
+    # we can override any part of the primary colors of the menu
+    #over_theme = {'txc_inactive': '#FFFFFF','menu_background':'red','txc_active':'yellow','option_active':'blue'}
+    over_theme = {'txc_inactive': '#FFFFFF'}
+    menu_id = hc.nav_bar(menu_definition=menu_data,home_name='Home',override_theme=over_theme)
+
+    #get the id of the menu item clicked
+    st.info(f"{menu_id=}")
+
     # Page title                   
     st.title("Facebook ad Report :bar_chart:")
 
