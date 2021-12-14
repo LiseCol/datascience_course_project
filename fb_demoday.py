ea178bd397e487b68852bd06f18601b880e5d2b4
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-14 21:14:12 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -23,7 +23,6 @@ st.markdown( """ <style> .css-1d391kg
                 </style> """, unsafe_allow_html=True, )
 st.markdown( """
 <nav>
-        <input type="checkbox" id="check">
         <label for="check" class="checkbtn">
             <i class="fas fa-bars"></i>
         </label>
