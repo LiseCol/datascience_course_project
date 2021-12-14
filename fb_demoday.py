5b3f16e8f7d3309a67271e07f78f6685c8615bba
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-15 00:31:15 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -156,7 +156,7 @@ def main():
                     go.Bar(x=ind_country['date'],
                     y=ind_country['spend'],
                     name="Spend"),
-                    colors=lightsteelblue,
+                    marker=dict(color='LightSkyBlue')
                     secondary_y=False,
                 )
         
