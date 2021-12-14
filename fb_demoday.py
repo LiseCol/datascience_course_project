53a683ff85d2284ee9ce291a674204b1a049e66d
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-14 23:09:37 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -114,7 +114,7 @@ def main():
         st.title(":arrow_left: Select a page on the side bar")
         #st.subheader(':arrow_left: Select a page on the side bar')
         image = Image.open('facebook-ads.png')
-        st.image(image, caption='Sunrise by the mountains',width=600)
+        st.image(image)
         
     ## Reporting per country
     if menu == 'Country Analysis':
