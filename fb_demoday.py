7d0ece3bd34e48a050e3ae6f40dbb5d73e361526
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-14 20:29:09 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -16,24 +16,6 @@ st.set_page_config(page_title="Facebook ad Report",
                    page_icon=":bar_chart:",
                    layout='wide',initial_sidebar_state='collapsed',)
 
-utl.remote_css('https://raw.githubusercontent.com/LiseCol/datascience_demo/main/css_bar_nav.css')
-
-st.markdown("""
-<nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">Logo</label>
-        <ul>
-            <li><a class="active" href="#">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">Feedback</a></li>
-        </ul>
-    </nav>    
-""",unsafe_allow_html=True)
 # Define functions
 @st.cache
 
