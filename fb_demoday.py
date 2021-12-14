8263485a0e1e79d8e6255a49f5708c6029009952
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-14 18:01:56 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -32,17 +32,10 @@ menu_id = hc.nav_bar(
     override_theme=over_theme,
     home_name='Home',
     login_name='Logout',
-    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+    hide_streamlit_markers=True, #will hide st hamburger 
     sticky_nav=True, #at the top or not
     sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
 )
-
-if st.button('click me'):
-  st.info('You clicked at: {}'.format(datetime.datetime.now()))
-
-
-if st.sidebar.button('click me too'):
-  st.info('You clicked at: {}'.format(datetime.datetime.now()))
 
 #get the id of the menu item clicked
 st.info(f"{menu_id}")
