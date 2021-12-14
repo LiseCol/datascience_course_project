415b2f312931d5f9457537363ba93ab18339cf3d
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-14 21:11:10 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -21,6 +21,22 @@ st.markdown( """ <style> .css-1d391kg
                 { background-color: rgb(237, 171, 173)
                 } 
                 </style> """, unsafe_allow_html=True, )
+st.markdown( """
+<nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">Logo</label>
+        <ul>
+            <li><a class="active" href="#">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Feedback</a></li>
+        </ul>
+    </nav>
+    """, unsafe_allow_html=True, )
 
 # Define functions
 @st.cache
