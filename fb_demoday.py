7923e3e0ef5db20f914422f20b81b220a5306289
LiseCol <94963620+LiseCol@users.noreply.github.com>
2021-12-15 14:26:21 +0100

    Update fb_demoday.py

--- fb_demoday.py
+++ fb_demoday.py
@@ -311,6 +311,7 @@ def main():
         
         
     if menu == 'Budget decision':
+        st.title('Optimal budget')
         status3 = st.select_slider("Select the prefered currency :",("Local","USD"))
         ## In local currency
         if status3 == "Local":
